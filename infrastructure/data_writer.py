2d7a0d826d256d1761b8ada1bc80cdd1145e788f
paupaupau <94736048+peredz@users.noreply.github.com>
2026-01-20 14:34:11 +1000

    feat(db): implement DBWriter class to write data to db, make docker-compose file to run postgres

---
+++ infrastructure/data_writer.py
@@ -0,0 +1,87 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+from typing import List
+from domain.entities.user import User
+from domain.entities.driver import Driver
+from domain.entities.ride import Ride
+
+
+class DBWriter:
+    """Класс для записи данных об объектах в Postgres"""
+    def __init__(self, host, port, database):
+        load_dotenv()
+        db_user = os.getenv('POSTGRES_USER')
+        db_password = os.getenv('POSTGRES_PASSWORD')
+
+        self.conn = psycopg2.connect(
+            host=host,
+            port=port,
+            database=database,
+            user=db_user,
+            password=db_password
+        )
+
+    def create_user_batch(self, users: List[User]):
+        """Метод для записи данных об пользователях в Postgres"""
+
+        with self.conn.cursor() as cursor:
+            for user in users:
+                cursor.execute(
+                    """
+                    INSERT INTO users (id, username, register_date)
+                    VALUES (%s, %s, %s)
+                    """,
+                    (
+                        user.id,
+                        user.username,
+                        user.register_date
+                    )
+                )
+        self.conn.commit()
+
+    def create_driver_batch(self, drivers: List[Driver]):
+        """Метод для записи данных о водителях в Postgres"""
+
+        with self.conn.cursor() as cursor:
+            for driver in drivers:
+                cursor.execute(
+                    """
+                    INSERT INTO drivers (id, username, license_plate, register_date)
+                    VALUES (%s, %s, %s, %s)
+                    """,
+                    (
+                        driver.id,
+                        driver.username,
+                        driver.license_plate,
+                        driver.register_date
+                    )
+                )
+        self.conn.commit()
+
+    def create_ride_batch(self, rides: List[Ride]):
+        """Метод для записи данных о поездке в Postgres"""
+
+        with self.conn.cursor() as cursor:
+            for ride in rides:
+                cursor.execute(
+                    """
+                    INSERT INTO rides (id, driver_id, rider_id, starting_point, destination, started_at, ended_at, price, tip)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    """,
+                    (
+                        ride.id,
+                        ride.driver_id,
+                        ride.rider_id,
+                        ride.starting_point,
+                        ride.destination,
+                        ride.started_at,
+                        ride.ended_at,
+                        ride.price,
+                        ride.tip
+                    )
+                )
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
